48817ad3bfd9bd547c5aa66b1c45d11c8a6f6169
Shaka Chen <scchen@shaka.today>
2022-09-04 22:50:53 +0800

    Changed reply-to to alist. The old API people can forgot the order.
    also some coding style tweaks

--- src/sendgrid.lisp
+++ src/sendgrid.lisp
@@ -51,63 +51,73 @@ The JSON looks like:
       list
       (list list)))
 
-(defun sendgrid-json (&key to from reply-to subject content-type content-value)
+(defun sendgrid-json (&key
+                        to
+                        from
+                        reply-to
+                        subject
+                        (content-type "text/plain") ; this duplication is a-must. &rest doesn't pass the default value of caller's keys.
+                        content-value
+                      &allow-other-keys)
   "Build the data json.
   `to': one email address or a list.
   `reply-to': a pair of email and name."
-  (unless (or nil (consp reply-to))
-    (error "\"reply-to\" must be a pair with an email and a name (strings)."))
-  (setf to (ensure-list to))
-  (let ((json-alist
-         `(("personalizations"
-            ,(loop for dest in to
-                collect `("to" (("email" . ,dest)))))
-           ("from" ("email" . ,from))
-           ("reply_to" ("email" . ,(car reply-to))
-                       ("name" . ,(cadr reply-to)))
-           ("subject" . ,subject)
-           ("content" (("type" . ,content-type)
-                       ("value" . ,content-value))))))
+  (assert (and to
+               from
+               subject
+               content-value))
+  (unless (or (null reply-to)
+              (and (stringp (cdr (assoc "email"
+                                        reply-to
+                                        :test #'string=)))
+                   (stringp (cdr (assoc "name"
+                                        reply-to
+                                        :test #'string=)))))
+    (error "\"reply-to\" must be an alist pair as ((\"email\" . string) (\"name\" . string))"))
+  (let* ((to (ensure-list to))
+         (json-alist
+           (append `(("personalizations"
+                      ,(loop for dest in to
+                             collect `("to" (("email" . ,dest)))))
+                     ("from" ("email" . ,from)))
+                   (when reply-to
+                     `(,(cons "reply_to" reply-to)))
+                   `(("subject" . ,subject)
+                     ("content" (("type" . ,content-type)
+                                 ("value" . ,content-value)))))))
     (jonathan:to-json json-alist :from :alist)))
 
 ;; test:
 #+nil
 (progn
   ;; Base case:
-  (assert (string-equal (sendgrid-json :to "to@mail" :from "me@mail" :subject "hello" :content-value "yo" :reply-to '("@" "me"))
+  (assert (string-equal (sendgrid-json :to "to@mail" :from "me@mail" :subject "hello" :content-value "yo" :reply-to '(("email" . "@") ("name" . "me")))
                         "{\"personalizations\":[{\"to\":[{\"email\":\"to@mail\"}]}],\"from\":{\"email\":\"me@mail\"},\"reply_to\":{\"email\":\"@\",\"name\":\"me\"},\"subject\":\"hello\",\"content\":[{\"type\":\"text/plain\",\"value\":\"yo\"}]}"))
 
   ;; With two receivers:
-  (assert (string-equal (sendgrid-json :to '("to@mail" "to-two@mail") :from "me@mail" :subject "hello" :content-value "yo" :reply-to '("@" "me"))
+  (assert (string-equal (sendgrid-json :to '("to@mail" "to-two@mail") :from "me@mail" :subject "hello" :content-value "yo" :reply-to '(("email" . "@") ("name" . "me")))
                         "{\"personalizations\":[{\"to\":[{\"email\":\"to@mail\"}],\"to\":[{\"email\":\"to-two@mail\"}]}],\"from\":{\"email\":\"me@mail\"},\"reply_to\":{\"email\":\"@\",\"name\":\"me\"},\"subject\":\"hello\",\"content\":[{\"type\":\"text/plain\",\"value\":\"yo\"}]}")))
 
-
-(defun send-email (&key
+(defun send-email (&rest rest
+                   &key
                      to
-                     (from (getf *email-config* :|from|))
-                     (reply-to (getf *email-config* :|reply-to|))
+                     from
                      subject
-                     (content-type "text/plain")
                      content
-  "Send an email with SendGrid's API.
-
-  -`from': from the `*email-config*' by default.
-  - `reply-to': must be a list with an email address and a name.
+                     reply-to
+                     (content-type "text/plain")
                      (api-key (uiop:getenv *api-key-environment-variable-name*))
                    &allow-other-keys) ; &allow-other-keys can help gradual API updates.
+  "Send an email with SendGrid's API. https://docs.sendgrid.com/api-reference/mail-send/mail-send#body Currently only supporting basic parameters for 80% use cases.
 
-  todo: make `to' accept multiple addresses."
-  (assert (and to from subject content))
+ Notice that `reply-to', if not nil, should be an alist as ((\"email\" . string) (\"name\" . string))"
   (dex:post *sendgrid-api*
             :headers `(("Authorization" . ,(concatenate
                                             'string
                                             "Bearer "
                                             api-key))
                        ("content-Type" . "application/json"))
-            :content (sendgrid-json :to to
-                                    :from from
-                                    :reply-to reply-to
-                                    :subject subject
-                                    :content-value content
-                                    :content-type content-type)))
             :verbose *verbose*
+            :content (apply #'sendgrid-json
+                            (append `(:content-value ,content)
+                                    rest)))) ; The compiler might warn variables defined but never used. But keeping the warnings should be better for future code modification. E.g., suppressing the warnings by (declare (ignorable ...) could result in debugging difficulties once the API changes.
