1f77aa706cf45f96e69391891da1a85e9ffbcafa
vindarel <vindarel@mailz.org>
2023-11-21 18:14:05 +0100

    test-sendgrid++ with attachment

--- src/test-sendgrid.lisp
+++ src/test-sendgrid.lisp
@@ -4,9 +4,9 @@
 ;; Fill config-test.lisp with your credential data.
 (load "config-test.lisp")
 
-(format t "Sending test email with config: ~a~&" *email-config*)
+(format t "Sending test email with config: ~%~%~{~a: ~s~^~&~}~&" *email-config*)
 
-(format t "Continue? [type any key to continue]~&")
+(format t "~%Continue? [enter any letter to continue or C-c]~&")
 
 (handler-case
     (read)
@@ -15,6 +15,7 @@
     (format t "Aborting.~&")
     (uiop:quit 1)))
 
+(format t "Sending… ")
 (sendgrid:send-email :api-key (getf *email-config* :|api-key|)
                      :to (getf *email-config* :|to|)
                      :from (getf *email-config* :|from|)
@@ -22,7 +23,11 @@
                      :subject "Sending emails from SendGrid is fun!"
                      :content-type "text/html"
                      :content "<h1>A title</h1><br/><strong>Sending emails from SendGrid is fun!</strong>"
+                     :attachments t
+		     :file "README.md"
+		     :filename "sendgrid-readme.markdown"
                      )
 
 
+(format t "done.~&")
 (uiop:quit)
